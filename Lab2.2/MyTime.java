81ef0290b6cda57a00e9fd6c642ad29524b5f816
Буров Владислав <burovvladislav4@gmail.com>
2025-10-20 11:37:02 +1000

    Update MyTime.java

--- Lab2.2/MyTime.java
+++ Lab2.2/MyTime.java
@@ -1,31 +1,19 @@
+import java.util.GregorianCalendar;
 public class MyTime {
-    public int hour = 0;
-    public int minute = 0;
-    public int second = 0;
+    private int hour = 0;
+    private int minute = 0;
+    private int second = 0;
     
     MyTime() {
+        Gregorian_Calendar now = new Gregorian_Calendar();
         
     }
     
-    Location(int x, int y, double value) {
-        row = x;
-        column = y;
-        maxValue = value;
+    MyTime(int h, int m, int s) {
+        hour = h;
+        minute = m;
+        second = s;
     }
     
-    public static Location locateLargest(double[][] a) {
-        double val = -1000000;
-        int r = 0;
-        int c = 0;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                if (a[i][j] > val) {
-                    val = a[i][j];
-                    r = i;
-                    c = j;
-                }
-            }
-        }
-        return new Location((r + 1), (c + 1), val);
-    }
+    
 }
