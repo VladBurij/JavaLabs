b9e2edf0db39338e643bbde92fad2317dd1ecc97
Буров Владислав <burovvladislav4@gmail.com>
2025-11-03 11:09:54 +1000

    Update MyTime.java

--- Lab2.2/MyTime.java
+++ Lab2.2/MyTime.java
@@ -1,19 +1,45 @@
-import java.util.GregorianCalendar;
 public class MyTime {
-    private int hour = 0;
-    private int minute = 0;
-    private int second = 0;
+  private int hour = 0;
+  private int minute = 0;
+  private int second = 0;
     
-    MyTime() {
-        Gregorian_Calendar now = new Gregorian_Calendar();
-        
-    }
+  MyTime() {
+    hour = (int)(System.currentTimeMillis() / 3600000);
+    minute = (int)(System.currentTimeMillis() % 3600000 / 60000);
+    second = (int)(System.currentTimeMillis() % 3600000 % 60000 / 1000);
+  }
     
-    MyTime(int h, int m, int s) {
-        hour = h;
-        minute = m;
-        second = s;
-    }
+  MyTime(long ms) {
+    hour = (int)(ms / 3600000);
+    ms %= 3600000;
+    minute = (int)(ms / 60000);
+    ms %= 60000;
+    second = (int)(ms / 1000);
+  }
     
+  MyTime(int h, int m, int s) {
+    hour = h;
+    minute = m;
+    second = s;
+  }
     
+  public int getHour() {
+    return hour;
+  }
+  
+  public int getMinute() {
+    return minute;
+  }
+  
+  public int getSecond() {
+    return second;
+  }
+  
+  public void setTime(long ms) {
+    hour = (int)(ms / 3600000);
+    ms %= 3600000;
+    minute = (int)(ms / 60000);
+    ms %= 60000;
+    second = (int)(ms / 1000);
+  }
 }
