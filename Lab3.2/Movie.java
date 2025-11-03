b74f0fd3a8f7a28cbc72469b941dfed96286d504
Буров Владислав <burovvladislav4@gmail.com>
2025-11-03 18:27:52 +1000

    Update Movie.java

--- Lab3.2/Movie.java
+++ Lab3.2/Movie.java
@@ -1,16 +1,12 @@
 public class Movie {
-  private String title = "";
-  private int duration = 0;
-  private double rating = 0.0;
+  public String title = "";
+  public int duration = 0;
+  public double rating = 0.0;
   
   Movie(String name, int dur, double rait) {
     title = name;
     duration = dur;
     rating = rait;
-  }
-  
-  public String toString(){
-    return "Название: " + title + ", Продолжительность: " + duration + ", Рейтинг: " + rating;
   }
   
   public boolean equals(Object another) {
@@ -26,5 +22,9 @@ public class Movie {
     else {
       return false;
     }
+  }
+  
+  public String toString() {
+    return "Название: " + title + ", Продолжительность: " + duration + ", Рейтинг: " + rating;
   }
 }
