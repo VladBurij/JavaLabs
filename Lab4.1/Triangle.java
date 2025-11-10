801081dd18b2c818122f34d6fbc3e2e12ab11314
Буров Владислав <burovvladislav4@gmail.com>
2025-11-10 18:36:22 +1000

    Update Triangle.java

--- Lab4.1/Triangle.java
+++ Lab4.1/Triangle.java
@@ -1,24 +1,28 @@
 public class Triangle {
-    public double A = 0;
-    public double B = 0;
-    public double C = 0;
+  public double A;
+  public double B;
+  public double C;
 
-    Triangle() {
-        A = 1;
-        B = 1;
-        C = 1;
+  Triangle() {
+    A = 1;
+    B = 1;
+    C = 1;
+  }
+
+  Triangle(double s1, double s2, double s3) throws IllegalTriangleException {
+    if (s1 >= s2 + s3) {
+      throw new IllegalTriangleException("Треугольник с такими сторонами не может существовать. Длина 1-й стороны больше суммы двух других.");
     }
-
-    Triangle(double s1, double s2, double s3) throws IllegalTriangleException {
-        try {
-            if (s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2) {
-                throw new IllegalTriangleException("Треугольник с такими сторонами не может сущестовать. Длина каждой стороны не должна превышать сумму двух других.")
-            }
-            else {
-                A = s1;
-                B = s2;
-                C = s3;
-            }
-        }
-    } 
+    else if (s2 >= s1 + s3) {
+      throw new IllegalTriangleException("Треугольник с такими сторонами не может существовать. Длина 2-й стороны больше суммы двух других.");
+    }
+    else if (s3 >= s1 + s2) {
+      throw new IllegalTriangleException("Треугольник с такими сторонами не может существовать. Длина 3-й стороны больше суммы двух других.");
+    }
+    else {
+      A = s1;
+      B = s2;
+      C = s3;
+    }
+  }
 }
