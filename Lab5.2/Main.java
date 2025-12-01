91b903683ec1ca33df0c3ce1e9d374883482b93d
Буров Владислав <burovvladislav4@gmail.com>
2025-12-01 11:09:35 +1000

    Update Main.java

--- Lab5.2/Main.java
+++ Lab5.2/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Main {
     public static Comparable[] MaxAndMin(Comparable[] array) throws Exception {
         if (array.length > 1) {
-            Arrays.sort(array, 0, array.length - 1);
+            Arrays.sort(array, 0, array.length);
             return new Comparable[] {array[array.length - 1], array[0]};
         }
         else {
@@ -11,7 +11,15 @@ public class Main {
     }
     
     public static void main(String args[]) {
-        int[] integers = {2, 1, 5, 8, 9, 3};
-        System.out.println(toString(MaxAndMin(integers)));
+        try {
+            Integer[] integers = {2, 1, 5, 8, 9, 13, -2, 3};
+            Comparable[] maxmin1 = MaxAndMin(integers);
+            String[] strings = {"корабль", "якорь", "листва", "акварель", "сова", "улица"};
+            Comparable[] maxmin2 = MaxAndMin(strings);
+            System.out.println(maxmin1[0] + ", " + maxmin1[1]);
+            System.out.println(maxmin2[0] + ", " + maxmin2[1]);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
