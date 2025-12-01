093571875d2653be9d4e63183d7d683b14ff02f5
Буров Владислав <burovvladislav4@gmail.com>
2025-12-01 11:13:54 +1000

    Update MyStack.java

--- Lab5.2/MyStack.java
+++ Lab5.2/MyStack.java
@@ -1,30 +1,31 @@
 import java.util.ArrayList;
 public class MyStack {
-  private ArrayList<Object> list = new ArrayList<>();
-  
-  public boolean isEmpty() {
-   return list.isEmpty();
-  }
-  
-  public int getSize() {
-    return list.size();
-  }
-  
-  public Object peek() {
-     return list.get(getSize() - 1);
-  }
-  
-  public Object pop() {
-    Object o = list.get(getSize() - 1);
-    list.remove(getSize() - 1);
-    return o;
-  }
-  
-  public void push(Object o) {
-    list.add(o);
-  }
-  
- @Override /** Переопределяет метод toString класса Object */
-  public String toString() {
-   return "стек: " + list.toString();
- } 
+    private ArrayList<Comparable> list = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public Object peek() {
+        return list.get(getSize() - 1);
+    }
+
+    public Object pop() {
+        Object o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+
+    public void push(Comparable o) {
+        list.add(o);
+    }
+
+    @Override /** Переопределяет метод toString класса Object */
+    public String toString() {
+        return "стек: " + list.toString();
+    }
+}
