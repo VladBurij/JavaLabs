38f18867104e3682cd48bdab0f5f94c38ecdf27d
Буров Владислав <burovvladislav4@gmail.com>
2025-12-08 11:03:18 +1000

    Update Main.java

--- Lab6.2/Main.java
+++ Lab6.2/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Sum of x+y = " + z);
+        ArrayList<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction(4, 6));
+        fractions.add(new Fraction(-1, 2));
+        fractions.add(new Fraction(6, -2));
+        fractions.add(new Fraction(4, 3));
+        Comparator comp = new FractionSortMaxMin();
+        Collections.sort(fractions, comp);
+        System.out.println(fractions);
     }
 }
